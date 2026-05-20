41f256a29188bb38a184947984dcc653eaa274d2
ziggy clout <allan.idaki@student.moringaschool.com>
2026-05-20 19:14:54 +0300

    feat(schedule): add route for parent schedule page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,12 +6,21 @@ import Register from './pages/Register';
 import ForgotPassword from './pages/ForgotPassword';
 import ResetPassword from './pages/ResetPassword';
 import Dashboard from './pages/Dashboard';
+import ParentSchedulePage from './pages/ParentSchedulePage';
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
         <Routes>
+          <Route 
+  path="/schedule"
+  element={
+    <ProtectedRoute>
+      <ParentSchedulePage />
+    </ProtectedRoute>
+  }
+/>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
